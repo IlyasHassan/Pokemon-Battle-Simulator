64d676807b7ac72fba179be602a95f1ad55a6839
Ilyas Hassan <ilyashassan4400@gmail.com>
2021-01-22 17:53:08 -0500

    Adding additional details

--- pokemonbattle/src/App.js
+++ pokemonbattle/src/App.js
@@ -1,11 +1,14 @@
 
 /*import './App.css';*/
+import { BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>Pokemon Battle</h1>
     </div>
+    </BrowserRouter>
   );
 }
 
