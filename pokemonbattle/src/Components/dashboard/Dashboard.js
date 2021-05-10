6a6513aa827921df5aee73aaadff7ffdfc0822e2
Ilyas Hassan <ilyashassan4400@gmail.com>
2021-05-10 10:07:54 -0400

    Improved the aesthetics of the website

--- pokemonbattle/src/Components/dashboard/Dashboard.js
+++ pokemonbattle/src/Components/dashboard/Dashboard.js
@@ -7,6 +7,7 @@ class Dashboard extends Component {
            <div className="row">
                <div className="col s12 m6"></div>
                <div className="col s12 m5 offset-m1"></div>
+               
            </div>
            </div>
     }
